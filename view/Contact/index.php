3df854db5aa88a1db02fec41687606f252dc6551
Bastien <bastien.meziere@protonmail.com>
2021-09-13 11:28:36 +0200

    Modification fichier view/Contact/index.php : Ajout du captcha Google

--- view/Contact/index.php
+++ view/Contact/index.php
@@ -13,27 +13,27 @@
       <form method="POST" action="mail/contact.php">
         <div class="col-md-6 form-group">
           <label class="labelcontact" for="prenom">Prénom *</label>
-          <input type="text" name="prenom" required="required" size="200" class="form-control form-control-lg">
+          <input id="prenom" type="text" name="prenom" required="required" size="200" class="form-control form-control-lg">
         </div>
         <div class="col-md-6 form-group">
           <label class="labelcontact" for="nom">Nom *</label>
-          <input type="text" name="nom" required="required" size="200" class="form-control form-control-lg">
+          <input id="nom" type="text" name="nom" required="required" size="200" class="form-control form-control-lg">
         </div>
     </div>
     <div class="row">
       <div class="col-md-6 form-group">
         <label class="labelcontact" for="email">Email *</label>
-        <input type="mail" name="email" required="required" class="form-control form-control-lg">
+        <input id="email" type="mail" name="email" required="required" class="form-control form-control-lg">
       </div>
       <div class="col-md-6 form-group">
         <label class="labelcontact" for="tel">Téléphone</label>
-        <input type="tel" name="tel" class="form-control form-control-lg">
+        <input id="tel" type="tel" name="tel" class="form-control form-control-lg">
       </div>
     </div>
     <div class="row">
       <div class="col-md-12 form-group">
         <label class="labelcontact" for="message">Message *</label>
-        <textarea name="message" required="required" cols="30" rows="12" class="form-control"></textarea>
+        <textarea id="message" name="message" required="required" cols="30" rows="12" class="form-control"></textarea>
       </div>
     </div>
     <div class="row">
@@ -43,14 +43,18 @@
         </div>
       </div>
     </div>
+    <input type="hidden" id="recaptchaResponse" name="recaptcha-response">
     <div class="row">
       <div class="col-12">
         <button id="submit" type="submit" class="btn btn-primary rounded-0 px-3 px-5">Envoyer</button>
       </div>
     </div>
+    </form>
+    <div class="row lamarge2">
+      <p>Ce site est protégé par reCAPTCHA et la <a href="https://policies.google.com/privacy?hl=fr" target="_blank">politique de confidentialité</a> et les <a href="https://policies.google.com/terms?hl=fr" target="_blank">conditions d'utilisation</a> de Google s'y appliquent.</p>
+    </div>
   </div>
 </div>
-
 <?php
   unset($_SESSION['success']);
 ?>
