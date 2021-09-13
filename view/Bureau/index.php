986cd5e0d387bf636577d772d75a750cc3fe836f
Bastien <bastien.meziere@protonmail.com>
2021-09-13 11:17:45 +0200

    Modification fichier Bureau/index.php : Optimisation foreach

--- view/Bureau/index.php
+++ view/Bureau/index.php
@@ -9,16 +9,14 @@
   
   <div class="container-fluid">
     <div class="row">
-      <?php
-            foreach($variabletrois['lebureau'] as $ligne) {
-      ?>
+      <?php foreach($variabletrois['lebureau'] as $ligne): ?>
     <div class="col-md-4 col-lg-4 col-sm-4 col-xs-12 text-center" data-aos="fade-up">
-      <img class="img-fluid w-50 rounded-circle mb-4" src="<?= IMAGE ?>images/<?= $ligne->img_photobureau?>" alt="Membre du bureau">
+      <img class="img-fluid w-50 rounded-circle mb-4" src="<?= IMAGE ?>images/<?= $ligne->img_photobureau?>" alt="Membre du bureau ES Bonchamp Tdt">
       <h2 class="text-black font-weight-light mb-4"><?= $ligne->nom_bureau ?></h2>
       <h2 class="text-black font-weight-light mb-4"><?= $ligne->role_bureau ?></h2>
       <p class="db mb-4"><?= $ligne->desc_bureau ?></p>
     </div>
-      <?php } ?>
+      <?php endforeach ?>
     </div>
   </div>
   </div>
